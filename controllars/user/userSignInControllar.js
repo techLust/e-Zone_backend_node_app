10faf86839306969b2c11b0a1019fbcba45d2ce1
mahatabhossain <official.mahatab21@gmail.com>
2023-04-04 16:14:53 +0530

    User sign in controller updated

--- controllars/user/userSignInControllar.js
+++ controllars/user/userSignInControllar.js
@@ -1,29 +1,59 @@
+require('dotenv').config();
 const UserModel = require('../../models/user/signUpUser');
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 const secretKey = process.env.SECRET_ACCESS_KEY;
 
 exports.signIn = async (req, res) => {
     try {
-        const { email, password } = req.body;
+        let { email, password } = req.body;
 
-        if (!email) return res.status(401).json({ message: "Email not found" });
-        if (!password) return res.status(401).json({ message: "Password not found" });
+        if (!email) return res.status(401).json({
+            message: "Email not found",
+            isError: true,
+        });
+        if (!password) return res.status(401).json({
+            message: "Password not found",
+            isError: true,
+        });
 
         const user = await UserModel.findOne({ email: email });
-        
-        if (email !== user.email) return res.status(401).json({ message: "unauthorized credentials" });
 
-        const isPasswordMatched = await bcrypt.compare(password, user.password)
-        if (!isPasswordMatched) return res.status(401).json({ message: "unauthorized credentials" });
+        if (!user) return res.status(500).json({
+            message: "User not found",
+            isError: true,
+        })
+
+        if (email !== user.email) return res.status(401).json({
+            message: "unauthorized credentials",
+            isError: true,
+        });
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+        
+        if (!isPasswordMatched) return res.status(401).json({
+            message: "unauthorized credentials",
+            isError: true,
+        });
+
+        password = null;
 
         //GENERATION TOKEN
-        const token = jwt.sign({ id:user._id }, secretKey);
+        const token = jwt.sign({ id: user._id }, secretKey);
+
         res.status(200).json({
-            message: "signin successful",
+            message: "User signed in successful",
             token: token,
             user: user,
+            isError: false,
         })
-    } catch { res.status(500).json({ message: "Unable to sign in" }) };
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({
+            message: err.message,
+            isError: true,
+        })
+    };
 }
